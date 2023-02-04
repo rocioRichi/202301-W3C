6fdce8fb643af9ff4c0e88da284079278c0bd6ad
rocioRichi <paragit@gmail.com>
2023-02-04 20:51:49 +0100

    add new timeorder and use of awaits

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,28 @@ import { Pokemon } from './models/poke-models/pokemon';
 
 const pokemonList = new PokemonList('main');
 
-fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
-  .then((response) => response.json())
-  .then((responseApi) => {
-    console.log(responseApi);
+const fetchPokemon = async (pokemon: { name: string; url: string }) => {
+  const response = await fetch(pokemon.url);
+  const pokemonApi = await response.json();
 
-    responseApi.results.forEach((pokemon: { name: string; url: string }) => {
-      fetchPokemon(pokemon);
-    });
-  });
-
-const fetchPokemon = (pokemon: { name: string; url: string }) => {
-  fetch(pokemon.url)
-    .then((response) => response.json())
-    .then((item) => {
-      console.log(item);
-      const pokemonModel = new Pokemon(
-        item.id,
-        item.name,
-        item.weight,
-        item.sprites.front_default,
-        []
-      );
-      new PokemonCard('ul', pokemonModel);
-    });
+  const pokemonModel = new Pokemon(
+    pokemonApi.id,
+    pokemonApi.name,
+    pokemonApi.weight,
+    pokemonApi.sprites.front_default,
+    []
+  );
+  new PokemonCard('ul', pokemonModel);
+  return pokemonModel;
 };
+
+async function getPokemon() {
+  const responseApi = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
+
+  const pokemonsApi = await responseApi.json();
+  const sortedPokemos = pokemonsApi.results.reverse();
+  for (const pokemonApi of sortedPokemos) {
+    await fetchPokemon(pokemonApi);
+  }
+}
+getPokemon();
