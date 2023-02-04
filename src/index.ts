eaf4e86ce0a88c4553cda6ce698e180f93ba45bd
rocioRichi <paragit@gmail.com>
2023-02-04 19:31:15 +0100

    Add fetch to Pokeapi

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import { PokemonList } from './components/pokemonList/pokemonList';
 
+fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+  .then((response) => response.json())
+  .then((pokemonList) => {
+    console.log(pokemonList);
+  });
